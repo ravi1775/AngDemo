7bfd1ca51a972ab59afc107602f010ebdc3beac6
RaviShankar <ravi_mindia@hotmail.com>
2021-09-19 12:57:06 +0530

    create user component added

---
+++ AngDemo/src/app/User.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template:'<h1>Create User</h1>',
+    selector: 'app-user'
+})
+export class User
+{
+
+}
--- AngDemo/src/app/app.module.ts
+++ AngDemo/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { User } from './User.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, User, CreateUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [User]
 })
 export class AppModule { }
